6a871a9bc6bad537fa8ffed5eab2e5d6375d70bb
ahmadkhairallah <Ahmad.khairallah94@gmail.com>
2024-08-20 21:35:57 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const heroButton = document.querySelector('.hero-button');
+    
+    heroButton.addEventListener('click', function() {
+        alert('تم الحجز بنجاح!');
+    });
+});
